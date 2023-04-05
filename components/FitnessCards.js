f24ab4d17f7520a81c55c4489340710f2f691b2a
DiogoPinhel <diogopinhel19@gmail.com>
2023-04-05 16:24:15 +0100

    Remove Stars in FitnessCards

--- components/FitnessCards.js
+++ components/FitnessCards.js
@@ -14,9 +14,6 @@ const FitnessCards = () => {
         <Pressable onPress={()=> navigation.navigate("Workout1",{image:item.image, excersises:item.excersises, id:item.id,})} style={{alignItems:"center", justifyContent:"center", margin:10}} key={key}>
             <Image style={{width:"95%", height:120, borderRadius:7}} source={{uri:item.image}}></Image>   
             <Text style = {{position:"absolute", color:"white", fontSize:16,fontWeight:"bold", left:20, top:25}}>{item.name}</Text> 
-            <AntDesign style = {{position:"absolute", color:"white", bottom:15, left:20}} name="star" size={18} />
-            <AntDesign style = {{position:"absolute", color:"white", bottom:15, left:40}} name="staro" size={18} />
-            <AntDesign style = {{position:"absolute", color:"white", bottom:15, left:60}} name="staro" size={18} />
         </Pressable>
         ))}
 
