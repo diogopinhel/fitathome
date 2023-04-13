9e0394afda937549b5d7c748b74d4facbd650d26
DiogoPinhel <diogopinhel19@gmail.com>
2023-04-13 12:48:31 +0100

    Update WorkoutScreen Name

--- components/FitnessCards.js
+++ components/FitnessCards.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { AntDesign } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import FitnessData from '../data/FitnessData';
+
 const FitnessCards = () => {
     const navigation = useNavigation();
     return (
@@ -11,7 +12,7 @@ const FitnessCards = () => {
         //AntDesign é as estrelas consoante a dificuldade
     <View style={{marginTop:60}}> 
         {FitnessData.map((item,key) => (
-        <Pressable onPress={()=> navigation.navigate("Workout1",{image:item.image, exercises:item.exercises, id:item.id,})} style={{alignItems:"center", justifyContent:"center", margin:10}} key={key}>
+        <Pressable onPress={()=> navigation.navigate("WorkoutScreen",{image:item.image, exercises:item.exercises, id:item.id,})} style={{alignItems:"center", justifyContent:"center", margin:10}} key={key}>
             <Image style={{width:"95%", height:120, borderRadius:7}} source={{uri:item.image}}></Image>   
             <Text style = {{position:"absolute", color:"white", fontSize:16,fontWeight:"bold", left:20, top:25}}>{item.name}</Text> 
         </Pressable>
