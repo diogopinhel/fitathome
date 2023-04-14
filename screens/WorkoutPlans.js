e56f3dfd5d0f862b35a24823aa0174c32d0b8376
DiogoPinhel <diogopinhel19@gmail.com>
2023-04-14 16:57:14 +0100

    #5 Update Design WorkoutPlans

--- screens/WorkoutPlans.js
+++ screens/WorkoutPlans.js
@@ -1,34 +1,32 @@
-import { StyleSheet, Text, View, ScrollView, Pressable } from "react-native";
+import { StyleSheet, Text, View, ScrollView, Pressable, Image } from "react-native";
 import React from "react";
 import FitnessCards from "../components/FitnessCards";
 import { useNavigation } from "@react-navigation/native";
-import { ImageBackground } from "react-native-web";
 
 const WorkoutPlans = () => {
   const navigation = useNavigation();
   return (
     <ScrollView style={{}}>
-     <ImageBackground   style={styles.backgroundImage}
-      source={{ uri: 'https://img.freepik.com/fotos-gratis/homem-jovem-fitness-em-estudio_7502-5008.jpg' }}>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            top: 60,
-          }}
-        >
-        </View>
-        <FitnessCards></FitnessCards>
-        </ImageBackground>
+      <View style={styles.container}>
+        <Text style={styles.heading}>Planos</Text>
+        <FitnessCards />
+      </View>
     </ScrollView>
   );
 };
+
 export default WorkoutPlans;
 
 const styles = StyleSheet.create({
-  backgroundImage: {
+  container: {
     flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
+    paddingHorizontal: 20,
+    paddingTop: 80,
+    backgroundColor: "#f0f0f0",
+  },
+  heading: {
+    fontSize: 32,
+    fontWeight: "bold",
+    marginBottom: 30,
   },
 });
