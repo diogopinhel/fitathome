74cb450ae439af8888e61474db279294642cf640
DiogoPinhel <diogopinhel19@gmail.com>
2023-04-19 15:40:03 +0100

    #5 Trade names ingleses to Portuguese
    troquei nomes em ingles para portugues com o Done para Seguinte e assim

--- screens/FitScreen.js
+++ screens/FitScreen.js
@@ -80,7 +80,7 @@ const FitScreen = () => {
             color: "white",
           }}
         >
-          DONE
+          SEGUINTE
         </Text>
       </Pressable>
       <Pressable
@@ -158,7 +158,7 @@ const FitScreen = () => {
               }}
             >
               {" "}
-              SEGUINTE{" "}
+              SALTAR{" "}
             </Text>
           </Pressable>
         )}
